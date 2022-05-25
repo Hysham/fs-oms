c200ee5c33170c172936f450ac0fc00fd263732c
Haisam Mohamed Hashim <hyshamhashim@gmail.com>
2022-05-25 18:17:45 +0100

    data loading

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -1,5 +1,5 @@
 'use strict';
-
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
 /**
  * An asynchronous bootstrap function that runs before
  * your application gets started.
@@ -10,4 +10,20 @@
  * See more details here: https://strapi.io/documentation/developer-docs/latest/setup-deployment-guides/configurations.html#bootstrap
  */
 
-module.exports = () => {};
+module.exports = async () => {
+    const data = require('../../data.json');
+    // data.Districts.forEach(el => {
+    //     strapi.query('districts').create({
+    //         district: el.district
+    //     }).then(d => {
+    //         let serviceAreas = data.Service_Area.filter(a => a.district === `${el.id}`)
+    //         // console.log(serviceAreas)
+    //         serviceAreas.forEach(sarea => {
+    //             strapi.services.areas.create({
+    //                 area: sarea["Service area"],
+    //                 district: d._id
+    //             })
+    //         })
+    //     })
+    // })
+};
