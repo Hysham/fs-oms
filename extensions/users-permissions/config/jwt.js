7df6a21630e860c1ccf328ec20c1eebbd3499858
Haisam Mohamed Hashim <hyshamhashim@gmail.com>
2022-05-25 16:00:27 +0100

    jwt.js updated

--- extensions/users-permissions/config/jwt.js
+++ extensions/users-permissions/config/jwt.js
@@ -1,7 +1,3 @@
 module.exports = {
-<<<<<<< HEAD
   jwtSecret: process.env.JWT_SECRET || 'd0260484-ff94-4084-a410-ce981bcfcf6c'
-=======
-  jwtSecret: process.env.JWT_SECRET || '236b6a25-073e-4e9f-af40-1e5c9476bed3'
->>>>>>> e2da87e4deae8b9b0fd432903c2967fdd2598f7a
 };
